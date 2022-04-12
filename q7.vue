0fb89170ccae355705deed9d01e9405a2a951324
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-12 17:23:48 +0530

    Add files via upload

---
+++ q7.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>table</h2>
+    <button @click="open()">open</button>
+    <p>{{opendata}}</p>
+    <button @click="pending()">pending</button>
+    <p>{{pendingdata}}</p>
+    <p>{{pendingdata1}}</p>
+    <button @click="closed()">closed</button>
+    <p>{{closeddata}}</p>
+  </div>
+</template>
+<script>
+export default{
+    name:"amrW3",
+    data(){
+        return{
+            data :[
+            {"email": "Eliseo@gardner.biz","id": 1,"name": "id labore ex et quamlaborum","status":"open"}, 
+            {"email": "Jayne_Kuhic@sydney.com","id": 2,"name": "quovero reiciendis velit similiqueearum","status":"pending"}, 
+            {"email": "Nikita@garfield.biz","id": 3,"name":"odioadipisci rerum autanimi","status":"closed"},
+            {"email": "Lew@alysha.tv","id": 4,"name": "alias odio sit","status":"pending"},
+            ],
+            opendata:"",
+            pendingdata:"",
+            pendingdata1:"",
+            closeddata:"",
+            text:""
+        }
+        },
+    methods:{
+        open(){
+            this.opendata = this.data[0];
+        },
+        pending(){
+            this.pendingdata = this.data[1];
+            this.pendingdata1 = this.data[3];
+        },
+        closed(){
+            this.closeddata = this.data[2];
+        }
+    }
+}
+</script>
---
+++ q8.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h3>UI with p-tag</h3>
+    <p @click="result()">p-tag</p>
+    <textarea id="sample"></textarea>
+    </div>    
+</template>
+<script>
+export default{
+    name:"amruWR2",
+    data(){
+        return{
+            text:"",
+        }
+    },
+        methods:{
+          result(){
+              this.text = document.write("" + document.grtElementById("sample").value);
+          },
+        }
+}
+</script>
